3fe01d7a64455d101ca95901c123c585bd770365
ZeroK ZK <Jose.GeeksJose@gmail.com>
2023-04-26 22:30:26 -0500

    animate text

--- components/SliderPages/GenerateMesh.jsx
+++ components/SliderPages/GenerateMesh.jsx
@@ -9,13 +9,13 @@ export const title = 'Paletas de colores'
 const GenerateMesh = () => {
     return (
         <div className='w-full h-full flex justify-center items-center'>
-            {title.split(' ').map((letter, i) => <motion.span
+            {title.split('').map((letter, i) => <motion.span
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0, transition: { delay: i * 0.01, ease: [0.2, 0.65, 0.3, 0.9] } }}
                 key={i}
-                className='text-5xl font-extrabold uppercase text-red-600'
+                className='text-6xl font-extrabold uppercase text-red-600'
             >
-                {letter}
+                {letters}
             </motion.span>)}
         </div>
     )
