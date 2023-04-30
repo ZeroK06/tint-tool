def38c7418be2830353fb3ccaacfe51ca1252ed9
ZeroK06 <jose.geeksjose@gmail.com>
2023-04-29 23:01:16 -0500

    animate colors in the home

--- components/SliderPages/GenerateMesh.jsx
+++ components/SliderPages/GenerateMesh.jsx
@@ -1,23 +1,30 @@
 'use client'
+import { randomColor } from '@/services/colorRandom'
 import { motion } from 'framer-motion'
 import React from 'react'
 
 
-export const title = 'Paletas de colores'
+export const title = 'Generador de mesh'
 
 
 const GenerateMesh = () => {
     return (
-        <div className='w-full h-full flex justify-center items-center'>
-            {title.split('').map((letter, i) => <motion.span
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0, transition: { delay: i * 0.01, ease: [0.2, 0.65, 0.3, 0.9] } }}
-                key={i}
-                className='text-6xl font-extrabold uppercase text-red-600'
-            >
-                {letters}
-            </motion.span>)}
-        </div>
+        <div className='w-full h-full flex justify-center items-center flex-col gap-12'>
+            <div className="title flex h-52">
+                {title.split('').map((letter, i) => letter != ' ' ? < motion.span
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0, transition: { delay: i * 0.01, ease: [0.2, 0.65, 0.3, 0.9] } }}
+                    key={i}
+                    className='text-6xl font-extrabold uppercase text-red-600'
+                >
+                    {letter}
+                </motion.span> : <span className='text-6xl font-extrabold uppercase text-red-600'>&nbsp;</span>)
+                }
+            </div>
+            <div className='w-full grid grid-cols-5 gap-3'>
+                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((e, index) => <div key={index * 5} className='rounded-xl h-44 w-full border relative' style={{ background: randomColor(), translateY: `${index * 40}px` }}></div>)}
+            </div>
+        </div >
     )
 }
 
--- components/SliderPages/GeneratePalette.jsx
+++ components/SliderPages/GeneratePalette.jsx
@@ -2,21 +2,24 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-export const title = 'Generador de mesh'
+export const title = 'Paleta de colores'
 
 
 const GeneratePalette = () => {
     return (
         <div className='w-full h-full flex justify-center items-center'>
-            {title.split(' ').map((e, i) => <motion.span
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0, transition: { delay: i * 0.01, ease: [0.2, 0.65, 0.3, 0.9] } }}
-                key={i}
-                className='text-5xl font-extrabold uppercase'
-            >
-                {e}
-            </motion.span>)}
-        </div>
+            <div className="title flex">
+                {title.split('').map((letter, i) => letter != ' ' ? < motion.span
+                    initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: 1, y: 0, transition: { delay: i * 0.01, ease: [0.2, 0.65, 0.3, 0.9] } }}
+                    key={i}
+                    className='text-6xl font-extrabold uppercase text-red-600'
+                >
+                    {letter}
+                </motion.span> : <span className='text-6xl font-extrabold uppercase text-red-600'>&nbsp;</span>)
+                }
+            </div>
+        </div >
     )
 }
 
