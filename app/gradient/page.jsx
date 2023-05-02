f10657b95163650bc18143d7f62cd6b3d74a8bed
ZeroK ZK <Jose.GeeksJose@gmail.com>
2023-05-01 22:35:01 -0500

    implementacion de controles del generador de gradientes - colores

--- app/gradient/page.jsx
+++ app/gradient/page.jsx
@@ -1,28 +1,54 @@
 'use client'
 
+import Wrap from '@/components/Wrap'
 import { randomColor } from '@/services/colorRandom'
 import React, { useEffect, useState } from 'react'
+import { BsPlus } from 'react-icons/bs'
 
 const page = () => {
     const [colors, setColors] = useState([])
+    const [isCompleteColors, setisCompleteColors] = useState(true)
     const [direction, setDirection] = useState(0)
     const [ruleCss, setRuleCss] = useState('')
     /* const handleRuleCss = ({ direction, colors = [] }) => {
         setRuleCss(`linear-gradient( ${direction ? direction + ',' : ''} ${colors.join(',')})`)
     }
 */
+    const handleColorsAdd = () => {
+        setColors([...colors, '#ffffff'])
+        if (colors.length == 4) {
+            setisCompleteColors(false)
+        }
+    }
+    const handleColor = e => {
+        console.log(e.target.getAttribute('color-position'))
+    }
     useEffect(() => {
         setColors(() => ([randomColor(), randomColor()]))
     }, [])
 
-
     return (
-        <div className="gradient relative" style={{ height: 'calc(100vh - 80px)', background: `linear-gradient( ${direction}deg, ${colors.join(',')})` }}>
-            <div className="controls absolute bottom-10 h-96 w-full flex justify-center items-center">
-                <div className='bg-white h-96 w-2/3 rounded-xl'>
-                    <input type="range" min={0} max={360} value={direction} onChange={(e) => setDirection(e.target.value)} />
-                </div>
-            </div>
+        <div className="gradient-maker min-h-screen w-full selection:text-blue-500 selection:bg-slate-500 selection:bg-opacity-20">
+            <Wrap>
+                <h1 className='text-5xl font-black text-slate-600 text-center mt-14'> Creador de Gradiantes</h1>
+                <p className='text-lg font-normal text-slate-500 text-center mt-4 mb-20'>Crear y exporta hermosas gradientes.</p>
+                <section className='gradient grid grid-cols-2 gap-10 w-2/3 mx-auto select-none'>
+                    <div className='controls border border-slate-300 rounded-2xl p-4'>
+                        <div className="colors grid grid-cols-5 gap-4">
+                            {colors?.map((color, index) => <div key={index} color-position={index} onClick={handleColor} className=' cursor-pointer border border-slate-200 p-1 rounded-lg h-10' style={{ background: color }}></div>)}
+                            {isCompleteColors && <div onClick={handleColorsAdd} className='cursor-pointer border border-slate-200 p-1 rounded-lg flex justify-center items-center'><BsPlus size={20} color='#696969' /></div>}
+                        </div>
+                        <div className="range">
+                            <input className='w-full' value={direction} min={0} max={360} onChange={e => setDirection(e.target.value)} type="range" />
+                        </div>
+                    </div>
+                    <div className='preview h-96 rounded-2xl overflow-hidden'>
+                        <div className='h-full w-full ' style={{ background: `linear-gradient( ${direction + 'deg,'} ${colors.join(',')})` }}>
+
+                        </div>
+                    </div>
+                </section>
+            </Wrap>
         </div>
     )
 }
---
+++ components/Wrap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wrap = ({ children }) => {
+    return (
+        <div className='w-5/6 mx-auto py-8'>{children}</div>
+    )
+}
+
+export default Wrap
